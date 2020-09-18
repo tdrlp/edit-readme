a2bcbec0600a8d3e28a16b4b0f7cddfd402435e6
isoyute <tudor.s.lupu@gmail.com>
2020-09-18 09:07:11 -0400

    Updates

---
+++ precache-manifest.15422e2519113db1607f0b1a717951bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff4c50629af946da2a56e058c73c2a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c765add5703977e3eb",
+    "url": "/static/js/main.13756c8b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
